9d4fbfcde3e1f6ddedb0329398b97ccdd41f7cb3
ssggii <haan7525@gmail.com>
2024-06-23 17:49:14 +0900

    [FEAT] BaseTimeEntity 생성

--- src/main/java/com/server/youthtalktalk/YouthTalkTalkApplication.java
+++ src/main/java/com/server/youthtalktalk/YouthTalkTalkApplication.java
@@ -2,8 +2,10 @@ package com.server.youthtalktalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YouthTalkTalkApplication {
 
 	public static void main(String[] args) {
