bfad377107f28d5f9bfa326a8ffdace7c0fb0fb8
HideOnCodec <tlsdmsgp33@naver.com>
2024-07-02 14:46:46 +0900

    [FIX] 테스트 수정

--- src/test/java/com/server/youthtalktalk/JenkinsTest.java
+++ src/test/java/com/server/youthtalktalk/JenkinsTest.java
@@ -8,12 +8,12 @@ public class JenkinsTest {
     @Test
     @DisplayName("테스트1")
     void pipelineTest1() {
-        System.out.println("Hello unit test");
+        System.out.println("Hello Unit Test");
     }
 
     @Test
     @DisplayName("테스트2")
     void pipelineTest2() {
-        System.out.println("Hello unit test2");
+        System.out.println("Hello Unit Test2");
     }
 }
