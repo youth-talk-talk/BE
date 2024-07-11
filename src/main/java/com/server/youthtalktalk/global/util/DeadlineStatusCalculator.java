cc4e002c5c347f422efa337e96b5a98be3f7934d
Joy <munchinthepool64@gmail.com>
2024-07-11 19:41:42 +0900

    [FIX] 마감 디데이 계산 로직 수정

--- src/main/java/com/server/youthtalktalk/global/util/DeadlineStatusCalculator.java
+++ src/main/java/com/server/youthtalktalk/global/util/DeadlineStatusCalculator.java
@@ -9,19 +9,21 @@ import java.time.temporal.ChronoUnit;
 public class DeadlineStatusCalculator {
 
     // 마감일 입력
-    // 마감상태 반환 (상시, D-day, 마감)
+    // 마감상태 반환 (D-00, D-DAY, 마감)
 
     public static String calculateDeadline(LocalDate applyDue) {
         if (applyDue == null) {
-            return "상시";
+            return ""; // 상시
         }
 
         LocalDate today = LocalDate.now();
         long daysBetween = ChronoUnit.DAYS.between(today, applyDue);
-        if (daysBetween < 0) {
-            return "마감";
-        } else {
+        if (daysBetween > 0) {
             return "D-" + daysBetween;
+        } else if (daysBetween == 0) {
+            return "D-DAY";
+        } else {
+            return "마감";
         }
     }
 
