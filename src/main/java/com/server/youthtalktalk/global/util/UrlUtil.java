1b37efd6581cb913b8746c607c9f44529da125ad
Joy <munchinthepool64@gmail.com>
2024-08-08 02:12:13 +0900

    [REFACTOR] URL 전처리 로직 의미 없는 변수 제거

--- src/main/java/com/server/youthtalktalk/global/util/UrlUtil.java
+++ src/main/java/com/server/youthtalktalk/global/util/UrlUtil.java
@@ -10,13 +10,10 @@ public class UrlUtil {
 
     public static String formatUrl(String url){
 
-        // 괄호 제거
-        String formattedUrl = url.replaceAll("[()]", " ");
-
         // url 추출
         String urlPattern = "((http[s]?|ftp)://)?(?:www\\.)?[-a-zA-Z0-9@:%._+~#=가-힣]{1,256}[:|.][a-zA-Z0-9]{1,6}\\b(?:[-a-zA-Z0-9@:%_+,.~#?&/=가-힣]*)";
         Pattern pt = Pattern.compile(urlPattern);
-        Matcher mc = pt.matcher(formattedUrl);
+        Matcher mc = pt.matcher(url.replaceAll("[()]", " "));
 
         return mc.find() ? mc.group() : "";
 
