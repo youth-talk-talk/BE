3586c28e8e4a6722bd7ca2d089017a2640f20ba8
Joy <munchinthepool64@gmail.com>
2024-08-25 22:52:53 +0900

    [FIX] PolicyListResponseDto builder 사용으로 변경

--- src/main/java/com/server/youthtalktalk/dto/policy/PolicyListResponseDto.java
+++ src/main/java/com/server/youthtalktalk/dto/policy/PolicyListResponseDto.java
@@ -3,9 +3,11 @@ package com.server.youthtalktalk.dto.policy;
 import com.server.youthtalktalk.domain.policy.Category;
 import com.server.youthtalktalk.domain.policy.Policy;
 import com.server.youthtalktalk.global.util.DeadlineStatusCalculator;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class PolicyListResponseDto {
 
     private String policyId; // 정책 아이디
@@ -14,15 +16,16 @@ public class PolicyListResponseDto {
     private String deadlineStatus; // 마감 상태
     private String hostDep; // 주관 기관명
     private boolean isScrap; // 스크랩 여부
+    private Long totalCount; // 총 정책 수
 
     public static PolicyListResponseDto toListDto(Policy policy, Boolean isScrap) {
-        PolicyListResponseDto dto = new PolicyListResponseDto();
-        dto.setPolicyId(policy.getPolicyId());
-        dto.setCategory(policy.getCategory());
-        dto.setTitle(policy.getTitle());
-        dto.setDeadlineStatus(DeadlineStatusCalculator.calculateDeadline(policy.getApplyDue()));
-        dto.setHostDep(policy.getHostDep());
-        dto.setScrap(isScrap);
-        return dto;
+        return PolicyListResponseDto.builder()
+                .policyId(policy.getPolicyId())
+                .category(policy.getCategory())
+                .title(policy.getTitle())
+                .deadlineStatus(DeadlineStatusCalculator.calculateDeadline(policy.getApplyDue()))
+                .hostDep(policy.getHostDep())
+                .isScrap(isScrap)
+                .build();
     }
 }
