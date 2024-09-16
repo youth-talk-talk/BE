f9c455e69a10370ee20f2e12febf1e674c6bfc2f
ssggii <haan7525@gmail.com>
2024-09-16 17:28:59 +0900

    [FIX] 소셜로그인 비밀번호 랜덤 생성으로 변경

--- src/main/java/com/server/youthtalktalk/global/login/CustomAuthenticationProvider.java
+++ src/main/java/com/server/youthtalktalk/global/login/CustomAuthenticationProvider.java
@@ -18,12 +18,17 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     private final UserDetailsService userDetailsService;
     private final GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
 
+    /**
+     * 요청의 username이 db의 사용자 정보와 일치하는지 비교한다.
+     * 인증에 성공하면, UserDetail 정보를 기반으로 UsernamePasswordAuthenticationToken 객체를 생성하여 반환한다.
+     */
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getPrincipal().toString();
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
         return new UsernamePasswordAuthenticationToken(userDetails,
-                "password", authoritiesMapper.mapAuthorities(userDetails.getAuthorities()));
+                userDetails.getPassword(), authoritiesMapper.mapAuthorities(userDetails.getAuthorities()));
     }
 
     @Override
--- src/main/java/com/server/youthtalktalk/global/login/LoginService.java
+++ src/main/java/com/server/youthtalktalk/global/login/LoginService.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 /**
  *    파라미터로 username으로 DB에서 일치하는 Member를 찾고,
  *    해당 회원의 username과 Role을 담아 UserDetails의 User 객체를 생성한다.
@@ -24,14 +26,21 @@ public class LoginService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
+    /**
+     * username이 일치하는 사용자 정보가 있는지 검증한다 -> 있으면 로그인 성공, 없으면 로그인 실패
+     * 비밀번호는 인증 절차에 필요하지 않으므로 난수로 만든 다음 UserDetail 객체로 넘긴다.
+     */
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("로그인에 실패했습니다."));
 
+        String randomPassword = UUID.randomUUID().toString();
+
         return User.builder()
                 .username(member.getUsername())
-                .password("password")
+                .password(randomPassword)
                 .roles(member.getRole().name())
                 .build();
     }
