ac21356f26b70b2650dd29d960bf848160882665
ssggii <haan7525@gmail.com>
2024-09-22 16:44:05 +0900

    [FIX] 로그인 수정 - username 해시값으로 변경

--- src/main/java/com/server/youthtalktalk/global/response/BaseResponse.java
+++ src/main/java/com/server/youthtalktalk/global/response/BaseResponse.java
@@ -1,7 +1,6 @@
 package com.server.youthtalktalk.global.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.ToString;
