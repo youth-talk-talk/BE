335bbef2ecf8f213a60c564b5c806764843911b0
HideOnCodec <tlsdmsgp33@naver.com>
2024-12-29 19:16:43 +0900

    [FIX] Content 컬럼 타입 변경(TEXT)

--- src/main/java/com/server/youthtalktalk/domain/post/entity/Content.java
+++ src/main/java/com/server/youthtalktalk/domain/post/entity/Content.java
@@ -1,15 +1,26 @@
 package com.server.youthtalktalk.domain.post.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Content {
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     @Enumerated(EnumType.STRING)
     private ContentType type;
+
+    @Builder
+    public Content(String content, ContentType type) {
+        this.content = content;
+        this.type = type;
+    }
 }
