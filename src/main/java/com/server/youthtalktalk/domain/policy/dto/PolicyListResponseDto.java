199b1ba5bfcfd3ed8b7c00e6a0ccce99d71d4416
ssggii <haan7525@gmail.com>
2025-05-10 22:56:42 +0900

    [REFACTOR] PoliyListResponseDto.category를 Enum의 name(한글)으로 변경

--- src/main/java/com/server/youthtalktalk/domain/policy/dto/PolicyListResponseDto.java
+++ src/main/java/com/server/youthtalktalk/domain/policy/dto/PolicyListResponseDto.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 public class PolicyListResponseDto {
 
     private Long policyId; // 정책 아이디
-    private Category category; // 카테고리
+    private String category; // 카테고리
     private String title; // 정책명
     private String deadlineStatus; // 마감 상태
     private String hostDep; // 주관 기관명
@@ -23,7 +23,7 @@ public class PolicyListResponseDto {
     public static PolicyListResponseDto toListDto(Policy policy, Boolean isScrap, long scrapCount) {
         return PolicyListResponseDto.builder()
                 .policyId(policy.getPolicyId())
-                .category(policy.getCategory())
+                .category(policy.getCategory().getName())
                 .title(policy.getTitle())
                 .deadlineStatus(DeadlineStatusCalculator.calculateDeadline(policy.getApplyDue()))
                 .hostDep(policy.getHostDep())
